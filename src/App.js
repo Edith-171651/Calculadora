df13cd77daf5eb384d75912b387777b9b11f4391
Edith Ruiz <edith.ruiz300894@gmail.com>
2022-07-02 22:52:29 -0500

    This is my calculator_Edith

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Calculator from './components/calculator/Calculator';
+
+function App() {
+  return (
+    <h1 >
+      <Calculator/>
+    </h1>
+  );
+}
+
+export default App;
